5435007013e8fa550533be6f62224fef9b86aa37
antosan <sandeantony@gmail.com>
2016-05-07 23:09:42 +0200

    Complete Lab 4

--- items.js
+++ items.js
@@ -339,13 +339,22 @@ function ItemDAO(database) {
 
         // TODO replace the following two lines with your code that will
         // update the document with a new review.
-        var doc = this.createDummyItem();
-        doc.reviews = [reviewDoc];
+        // var doc = this.createDummyItem();
+        // doc.reviews = [reviewDoc];
+
+        this.db.collection('item').updateOne(
+            { _id: itemId },
+            { $push: { reviews: reviewDoc } },
+            function (err, doc) {
+                assert.equal(null, err);
+
+                callback(doc);
+            });
 
         // TODO Include the following line in the appropriate
         // place within your code to pass the updated doc to the
         // callback.
-        callback(doc);
+        // callback(doc);
     }
 
 
